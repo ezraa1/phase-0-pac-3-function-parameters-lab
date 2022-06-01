b65dacd9b1aa6472e8a6885d7244845d6c3decea
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-01 14:49:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function introductionWithLanguage(name,language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 console.log (introductionWithLanguage("Aki", "Ember.js"));
+
 function introductionWithLanguageOptional(name, language="JavaScript") {
 	return	`Hi, my name is ${name} and I am learning to program in ${language}.`
 
